ba900c9a9edca3f884edf2f0e36e6126f1ab6e64
pilipchuk-philip <ppy@csis.com>
2026-03-15 13:10:19 +0100

    darwin: work mac git update

--- home/darwin-work.nix
+++ home/darwin-work.nix
@@ -10,4 +10,9 @@
     name = "pilipchuk-philip";
     email = "ppy@csis.com";
   };
+
+  programs.git.settings.push.default = "current";
+  programs.git.settings.gpg.format = "ssh";
+  programs.git.settings.user.signingkey = "~/.ssh/id_ed25519.pub";
+  programs.git.settings.commit.gpgsign = true;
 }
