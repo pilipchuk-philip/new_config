d20a3be4bb1b2c15df80ef833655562c9c07aa96
pilipchuk-philip <pilipchuk.philip@gmail.com>
2026-07-07 23:51:25 +0200

    home: finish module split

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -28,6 +28,14 @@
       fd = "fd --hidden --color always -i ";
     };
     initContent = ''
+      if [ -d /etc/nixos/scripts ]; then
+        export PATH="/etc/nixos/scripts:$PATH"
+      elif [ -d "${config.home.homeDirectory}/new_config/scripts" ]; then
+        export PATH="${config.home.homeDirectory}/new_config/scripts:$PATH"
+      fi
+      export PATH="$(npm config get prefix)/bin:$PATH"
+      eval "$(uv generate-shell-completion zsh)"
+      eval "$(uvx --generate-shell-completion zsh)"
       export POWERLEVEL9K_CONFIG_FILE=${config.xdg.configHome}/p10k.zsh
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       source ${config.xdg.configHome}/p10k.zsh
